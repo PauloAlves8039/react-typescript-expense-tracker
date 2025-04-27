7bd9c920eba1fe9eb19b63df8a03fd41a4ddb39d
PauloAlves8039 <pj38alves@gmail.com>
2025-04-27 14:17:16 -0300

    feat: add style components for body header and header text

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ export default function App() {
   return (
     <>
       <C.Container>
-        ...
+        <C.Header>
+          <C.HeaderText>Financial System</C.HeaderText>
+        </C.Header>
+        <C.Body>
+          ...
+        </C.Body>
       </C.Container>
     </>
   )
