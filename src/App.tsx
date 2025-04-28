749faa5f0df4ff12f63d5d079565a1bc761c059f
PauloAlves8039 <pj38alves@gmail.com>
2025-04-28 20:22:35 -0300

    feat: apply monitoring for list and current month

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 import * as C from "./App.styles";
+import { useState, useEffect } from "react";
+import { Item } from "./types/Item";
+import { Category } from "./types/Category";
+import { categories } from "./data/categories";
+import { items } from "./data/items";
+import { getCurrentMonth, filterListByMonth } from "./helpers/dateFilter";
 
 export default function App() {
+  const [list, setList] = useState(items);
+  const [ filteredList, setFilteredList ] = useState<Item[]>([]);
+  const [ currentMonth, setCurrentMonth ] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
+  
   return (
     <>
       <C.Container>
