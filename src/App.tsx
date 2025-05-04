702f18d84808e753a603eeceb0b6151965f6223b
PauloAlves8039 <pj38alves@gmail.com>
2025-05-04 14:44:30 -0300

    feat: define calculation for income and expenses

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,28 @@ export default function App() {
   const [list, setList] = useState(items);
   const [ filteredList, setFilteredList ] = useState<Item[]>([]);
   const [ currentMonth, setCurrentMonth ] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
 
   useEffect(() => {
     setFilteredList(filterListByMonth(list, currentMonth));
   }, [list, currentMonth]);
+
+  useEffect(() => {
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for (let i in filteredList) {
+      if (categories[filteredList[i].category].expense) {
+        expenseCount += filteredList[i].value;
+      } else {
+        incomeCount += filteredList[i].value;
+      }
+    }
+
+    setIncome(incomeCount);
+    setExpense(expenseCount);
+  }, [filteredList]);
 
   const handleMonthChange = (newMonth: string) => {
     setCurrentMonth(newMonth);
@@ -30,7 +48,9 @@ export default function App() {
         <C.Body>
           <InfoArea 
             currentMonth={currentMonth}
-            onMonthChange={handleMonthChange} 
+            onMonthChange={handleMonthChange}
+            income={income}
+            expense={expense} 
           />
           <TableArea list={filteredList} />
         </C.Body>
