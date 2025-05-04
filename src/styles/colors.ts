bf3392398601d359e3c02e005014b72c02230acb
PauloAlves8039 <pj38alves@gmail.com>
2025-05-04 14:34:35 -0300

    style: add new colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,3 +1,5 @@
 export const purpleColor1 = "#7A67EE";
 export const neutralColor1 = "#FFF";
 export const grayColor1 = "#CCC"; 
+export const grayColor2 = "#888"; 
+export const darkColor1 = "#000"; 
