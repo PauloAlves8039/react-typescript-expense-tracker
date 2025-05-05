7d35a489a54397dc9cec54a74b514676c27fe5ed
PauloAlves8039 <pj38alves@gmail.com>
2025-05-05 15:06:21 -0300

    style: update colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -3,3 +3,6 @@ export const neutralColor1 = "#FFF";
 export const grayColor1 = "#CCC"; 
 export const grayColor2 = "#888"; 
 export const darkColor1 = "#000"; 
+export const blueColor1 = "#ADD8E6"; 
+export const greenColor1 = "#43CD80"; 
+export const redColor1 = "#FF0000"; 
